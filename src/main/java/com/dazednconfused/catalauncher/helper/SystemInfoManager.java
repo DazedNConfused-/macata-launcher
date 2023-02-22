4e2022faf8f25cf2744deeac9fa30ad561c080cd
Gonza Lopez <gonzalo.lopez@wbconsultant.com>
2023-02-22 12:15:16 -0300

    wip

---
+++ src/main/java/com/dazednconfused/catalauncher/helper/SystemInfoManager.java
@@ -0,0 +1,145 @@
+package com.dazednconfused.catalauncher.helper;
+
+public class SystemInfoManager {
+
+    /**
+     * The version of Java Runtime Environment.
+     */
+    private static final String JAVA_VERSION = "java.version";
+
+    /**
+     * The name of Java Runtime Environment vendor.
+     * */
+    public static final String JAVA_VENDOR = "java.vendor";
+
+    /**
+     * The URL of Java vendor.
+     * */
+    public static final String JAVA_VENDOR_URL = "java.vendor.url";
+
+    /**
+     * The directory of Java installation.
+     * */
+    public static final String JAVA_HOME = "java.home";
+
+    /**
+     * The specification version of Java Virtual Machine.
+     * */
+    public static final String JAVA_VM_SPECIFICATION_VERSION = "java.vm.specification.version";
+
+    /**
+     * The name of specification vendor of Java Virtual Machine.
+     * */
+    public static final String JAVA_VM_SPECIFICATION_VENDOR = "java.vm.specification.vendor";
+
+    /**
+     * Java Virtual Machine specification name.
+     * */
+    public static final String JAVA_VM_SPECIFICATION_NAME = "java.vm.specification.name";
+
+    /**
+     * JVM implementation version.
+     * */
+    public static final String JAVA_VM_VERSION = "java.vm.version";
+
+    /**
+     * JVM implementation vendor.
+     * */
+    public static final String JAVA_VM_VENDOR = "java.vm.vendor";
+
+    /**
+     * JVM  implementation name
+     * */
+    public static final String JAVA_VM_NAME = "java.vm.name";
+
+    /**
+     * The name of specification version Java Runtime Environment
+     * */
+    public static final String JAVA_SPECIFICATION_VERSION = "java.specification.version";
+
+    /**
+     * JRE specification vendor.
+     * */
+    public static final String JAVA_SPECIFICATION_VENDOR = "java.specification.vendor";
+
+    /**
+     * JRE specification name.
+     * */
+    public static final String JAVA_SPECIFICATION_NAME = "java.specification.name";
+
+    /**
+     * Java class format version number.
+     * */
+    public static final String JAVA_CLASS_VERSION = "java.class.version";
+
+    /**
+     * Path of java class.
+     * */
+    public static final String JAVA_CLASS_PATH = "java.class.path";
+
+    /**
+     * List of paths to search when loading libraries.
+     * */
+    public static final String JAVA_LIBRARY_PATH = "java.library.path";
+
+    /**
+     * The path of temp file.
+     * */
+    public static final String JAVA_IO_TMPDIR = "java.io.tmpdir";
+
+    /**
+     * The Name of JIT compiler to use.
+     * */
+    public static final String JAVA_COMPILER = "java.compiler";
+
+    /**
+     * The path of extension directory or directories.
+     * */
+    public static final String JAVA_EXT_DIRS = "java.ext.dirs";
+
+    /**
+     * The name of OS name.
+     * */
+    public static final String OS_NAME = "os.name";
+
+    /**
+     * The OS architecture.
+     * */
+    public static final String OS_ARCH = "os.arch";
+
+    /**
+     * The version of OS.
+     * */
+    public static final String OS_VERSION = "os.version";
+
+    /**
+     * The File separator.
+     * */
+    public static final String FILE_SEPARATOR = "file.separator";
+
+    /**
+     * The path separator.
+     * */
+    public static final String PATH_SEPARATOR = "path.separator";
+
+    /**
+     * The line separator.
+     * */
+    public static final String LINE_SEPARATOR = "line.separator";
+
+    /**
+     * The name of account name user.
+     * */
+    public static final String USER_NAME = "user.name";
+
+    /**
+     * The home directory of user.
+     * */
+    public static final String USER_HOME = "user.home";
+
+    /**
+     * The current working directory of the user.
+     * */
+    public static final String USER_DIR = "user.dir";
+
+}
