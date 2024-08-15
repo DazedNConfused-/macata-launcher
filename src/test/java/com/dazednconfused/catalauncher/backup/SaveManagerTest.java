28b1244a25048fc768a9810f6bdb942004af6a0d
Gonza Lopez <gonzalo.lopez@wbdcontractor.com>
2024-08-15 18:23:14 -0300

    wip: save manager test [macata #61]

---
+++ src/test/java/com/dazednconfused/catalauncher/backup/SaveManagerTest.java
@@ -0,0 +1,69 @@
+package com.dazednconfused.catalauncher.backup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mockStatic;
+
+import com.dazednconfused.catalauncher.helper.Paths;
+import com.dazednconfused.catalauncher.utils.TestUtils;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.MockedStatic;
+
+class SaveManagerTest {
+
+    @Test
+    void list_all_backups_success(@TempDir Path mockedDirectory) {
+        try (MockedStatic<Paths> mockedPaths = mockStatic(Paths.class)) {
+
+            // prepare mock data ---
+            mockedPaths.when(Paths::getSaveBackupPath).thenReturn(mockedDirectory.toString());
+
+            // pre-test assertions ---
+            assertThat(new File(Paths.getSaveBackupPath())).exists();
+
+            File MOCKED_BACKUP_1 = new File(Paths.getSaveBackupPath(), "20230216_111637.zip");
+            FileUtils.copyFile(
+                TestUtils.getFromResource("save/backup/sample/20230216_111637.zip"),
+                MOCKED_BACKUP_1
+            );
+
+            File MOCKED_BACKUP_2 = new File(Paths.getSaveBackupPath(), "20230217_115559.zip");
+            FileUtils.copyFile(
+                TestUtils.getFromResource("save/backup/sample/20230217_115559.zip"),
+                MOCKED_BACKUP_2
+            );
+
+            File MOCKED_BACKUP_3 = new File(Paths.getSaveBackupPath(), "20240808_205649.zip");
+            FileUtils.copyFile(
+                TestUtils.getFromResource("save/backup/sample/20240808_205649.zip"),
+                MOCKED_BACKUP_3
+            );
+
+            File MOCKED_BACKUP_4 = new File(Paths.getSaveBackupPath(), "20240808_205736.zip");
+            FileUtils.copyFile(
+                TestUtils.getFromResource("save/backup/sample/20240808_205736.zip"),
+                MOCKED_BACKUP_4
+            );
+
+            // execute test ---
+            List<File> result = SaveManager.listAllBackups();
+
+            // verify assertions ---
+            assertThat(result).containsExactlyInAnyOrder(
+                MOCKED_BACKUP_1,
+                MOCKED_BACKUP_2,
+                MOCKED_BACKUP_3,
+                MOCKED_BACKUP_4
+            );
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
