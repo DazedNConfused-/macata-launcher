61ffeccee2e249f649de0af3ffc3031fcd678f2f
Gonza Lopez <gonzalo.lopez@wbdcontractor.com>
2025-07-17 16:28:28 -0300

    feat: bulk insertion for DAOs [macata #99]

--- src/main/java/com/dazednconfused/catalauncher/database/mod/repository/ModH2RepositoryImpl.java
+++ src/main/java/com/dazednconfused/catalauncher/database/mod/repository/ModH2RepositoryImpl.java
@@ -8,6 +8,7 @@ import com.dazednconfused.catalauncher.database.mod.entity.ModEntity;
 import com.dazednconfused.catalauncher.database.mod.entity.ModfileEntity;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -58,6 +59,25 @@ public class ModH2RepositoryImpl extends MigrateableH2Database implements ModRep
         );
 
         return result;
+    }
+
+    @Override
+    public int bulkInsert(Collection<ModEntity> entities) throws DAOException {
+        LOGGER.debug("Bulk inserting [{}] ModEntity(ies)...", entities.size());
+
+        int insertedCount = 0;
+
+        for (ModEntity entity : entities) {
+            ModEntity insertedEntity = this.insert(entity);
+            if (insertedEntity != null) {
+                this.insertChildEntities(insertedEntity.getId(), entity.getModfiles());
+                insertedCount++;
+            }
+        }
+
+        LOGGER.debug("Bulk inserted [{}] ModEntity(ies)", insertedCount);
+
+        return insertedCount;
     }
 
     @Override
@@ -121,10 +141,11 @@ public class ModH2RepositoryImpl extends MigrateableH2Database implements ModRep
     private List<ModfileEntity> insertChildEntities(long modId, List<ModfileEntity> entities) throws DAOException {
         LOGGER.debug("Inserting [{}] ModfileEntity(s) associated to modId [{}]", entities.size(), modId);
 
-        List<ModfileEntity> result = entities.stream()
+        modfileDAO.bulkInsert(entities.stream()
             .peek(e -> e.setModId(modId)) // set/overwrite with entity ID
-            .map(modfileDAO::insert)
-            .collect(Collectors.toList());
+            .collect(Collectors.toList())
+        );
+        List<ModfileEntity> result = modfileDAO.findAllByModId(modId);
 
         LOGGER.debug("Inserted [{}] ModfileEntity(s) associated to modId [{}]", result.size(), modId);
 
